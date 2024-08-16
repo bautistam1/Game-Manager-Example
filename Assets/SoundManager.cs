b5a72e31e4c1ee076c2a91ad134c0c41c54c6b68
bautistam1 <147621284+bautistam1@users.noreply.github.com>
2024-08-16 10:29:47 -0300

    fin

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    public AudioSource SourceSFX;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+
+    public void PlaySound(AudioClip clip)
+    {
+        SourceSFX.PlayOneShot(clip);
+    }
+}
